b014ae9f1c95c0670156b120ba7aa604d4ec01ab
Igor Rodrigues <igor.rodrigues@exati.com.br>
2025-11-09 16:12:41 -0300

    tutorial: bullets e life bar

--- main.py
+++ main.py
@@ -13,12 +13,21 @@ PLAYER_WIDTH = 42
 PLAYER_HEIGHT = 48
 PLAYER_JUMP_WIDTH = 52
 PLAYER_JUMP_HEIGHT = 60
+PLAYER_SHOOT_WIDTH = 62 # SAME HEIGHT
+PLAYER_JUMP_SHOOT_WIDTH = 58 # SAME JUMP HEIGHT
 PLAYER_DISTANCE = 5
 
 GRAVITY = 0.5
 FRICTION = 0.4
 PLAYER_VELOCITY_X = 5
 PLAYER_VELOCITY_Y = -12
+
+PLAYER_BULLET_WIDTH = 16
+PLAYER_BULLET_HEIGHT = 12
+PLAYER_BULLET_VELOCTIY_X = 8
+
+HEALTH_WIDTH = 16
+HEALTH_HEIGHT = 4
 
 # enemy variables
 METALL_WIDTH = 36
@@ -36,9 +45,14 @@ player_image_right = load_image("megaman-right.png", (PLAYER_WIDTH, PLAYER_HEIGH
 player_image_left = load_image("megaman-left.png", (PLAYER_WIDTH, PLAYER_HEIGHT))
 player_image_jump_right = load_image("megaman-right-jump.png", (PLAYER_JUMP_WIDTH, PLAYER_JUMP_HEIGHT))
 player_image_jump_left = load_image("megaman-left-jump.png", (PLAYER_JUMP_WIDTH, PLAYER_JUMP_HEIGHT))
+player_image_shoot_right = load_image("megaman-right-shoot.png", (PLAYER_SHOOT_WIDTH, PLAYER_HEIGHT))
+player_image_shoot_left = load_image("megaman-left-shoot.png", (PLAYER_SHOOT_WIDTH, PLAYER_HEIGHT))
+player_image_jump_shoot_right = load_image("megaman-right-jump-shoot.png", (PLAYER_JUMP_SHOOT_WIDTH, PLAYER_JUMP_HEIGHT))
+player_image_jump_shoot_left = load_image("megaman-left-jump-shoot.png", (PLAYER_JUMP_SHOOT_WIDTH, PLAYER_JUMP_HEIGHT))
+player_image_bullet = load_image("bullet.png", (PLAYER_BULLET_WIDTH, PLAYER_BULLET_HEIGHT))
 floor_tile_image = load_image("floor-tile.png", (TILE_SIZE, TILE_SIZE))
 metall_image_left = load_image("metall-left.png", (METALL_WIDTH, METALL_HEIGHT))
-
+health_image = load_image("health.png", (HEALTH_WIDTH, HEALTH_HEIGHT))
 pygame.init()
 window = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
 pygame.display.set_caption("Meu jogo")
@@ -46,7 +60,26 @@ pygame.display.set_icon(player_image_right)
 clock = pygame.time.Clock()
 
 INVINCIBLE_END = pygame.USEREVENT + 0
+SHOOTING_END = pygame.USEREVENT + 1
+
 class Player(pygame.Rect):
+    class Bullet(pygame.Rect):
+        def __init__(self):
+
+            if player.direction == "left":
+                pygame.Rect.__init__(self, player.x, player.y + TILE_SIZE / 2, PLAYER_BULLET_WIDTH, PLAYER_BULLET_HEIGHT)
+
+                self.velocity_x = -PLAYER_BULLET_VELOCTIY_X
+            
+            elif player.direction == "right":
+                pygame.Rect.__init__(self, player.x + player.width, player.y + TILE_SIZE / 2, PLAYER_BULLET_WIDTH, PLAYER_BULLET_HEIGHT)
+
+                self.velocity_x = PLAYER_BULLET_VELOCTIY_X
+            
+            self.image = player_image_bullet
+            self.used = False
+            
+
     def __init__(self):
         pygame.Rect.__init__(self, PLAYER_X, PLAYER_Y, PLAYER_WIDTH, PLAYER_HEIGHT)
         self.image = player_image_right
@@ -57,9 +90,23 @@ class Player(pygame.Rect):
         self.invincible = False
         self.max_health = 28
         self.health = self.max_health
-    
+        self.shooting = False
+        self.bullets = []
+
     def update_image(self):
-        if self.jumping:
+        if self.jumping and self.shooting:
+            if self.direction == "right":
+                self.image = player_image_jump_shoot_right
+            elif self.direction == "left":
+                self.image = player_image_jump_shoot_left
+
+        elif self.shooting:
+            if self.direction == "right":
+                self.image = player_image_shoot_right
+            elif self.direction == "left":
+                self.image = player_image_shoot_left
+
+        elif self.jumping:
             if self.direction == "right":
                 self.image = player_image_jump_right
             elif self.direction == "left":
@@ -73,6 +120,13 @@ class Player(pygame.Rect):
     def set_invincible(self, milliseconds=1000):
         self.invincible = True
         pygame.time.set_timer(INVINCIBLE_END, milliseconds, 1)
+
+    def set_shooting(self):
+        if not self.shooting:
+            self.shooting = True
+            self.bullets.append(Player.Bullet())
+            pygame.time.set_timer(SHOOTING_END, 250, 1)
+
 class Metall(pygame.Rect):
     def __init__(self, x, y):
         pygame.Rect.__init__(self, x, y, METALL_WIDTH, METALL_HEIGHT)
@@ -80,6 +134,8 @@ class Metall(pygame.Rect):
         self.velocity_y = 0
         self.direction = "left"
         self.jumping = False
+        self.health = 3
+
 class Tile(pygame.Rect):
     def __init__(self, x, y, image):
         pygame.Rect.__init__(self, x, y, TILE_SIZE, TILE_SIZE)
@@ -129,6 +185,8 @@ def check_tile_collision_y(character):
         character.velocity_y = 0
 
 def move():
+    global metalls
+
     # x movement
     if player.direction == "left" and player.velocity_x < 0:
         player.velocity_x += FRICTION
@@ -150,6 +208,18 @@ def move():
 
     check_tile_collision_y(player)
 
+    # bullets
+    for bullet in player.bullets:
+        bullet.x += bullet.velocity_x
+        for metall in metalls:
+            if metall.health > 0 and not bullet.used and bullet.colliderect(metall):
+                metall.health -= 1
+                bullet.used = True
+    
+    player.bullets = [bullet for bullet in player.bullets if not bullet.used \
+                      and bullet.x + bullet.width > 0 and bullet.x < GAME_WIDTH]
+    metalls = [metall for metall in metalls if metall.health > 0]
+
     # enemy y movement
     for metall in metalls:
         metall.velocity_y += GRAVITY
@@ -158,7 +228,9 @@ def move():
 
         if player.colliderect(metall) and not player.invincible:
              print("colisao com metall")
+             player.health -= 1
              player.set_invincible()
+
 
 
 # start game
@@ -172,13 +244,26 @@ def draw():
     window.fill((20, 18, 167))
     window.blit(background_image, (0, 80))
 
+    # tiles
     for tile in tiles:
         window.blit(tile.image, tile)
-
+    
+    # player
     player.update_image()
+
+    # enemys
     window.blit(player.image, player)
     for metall in metalls:
         window.blit(metall.image, metall)
+
+    # health bar
+    pygame.draw.rect(window, "black", (TILE_SIZE, TILE_SIZE, HEALTH_WIDTH, HEALTH_HEIGHT * player.max_health))
+    for i in range(player.max_health - player.health, player.max_health):
+        window.blit(health_image, (TILE_SIZE,  TILE_SIZE + i*HEALTH_HEIGHT, HEALTH_WIDTH, HEALTH_HEIGHT))
+
+    # bullets 
+    for bullet in player.bullets:
+        window.blit(bullet.image, bullet)
 
 while True: #game loop
     for event in pygame.event.get():
@@ -188,6 +273,9 @@ while True: #game loop
 
         if event.type == INVINCIBLE_END:
             player.invincible = False
+
+        elif event.type == SHOOTING_END:
+            player.shooting = False
 
     keys = pygame.key.get_pressed()
     if (keys[pygame.K_UP] or keys[pygame.K_w]) and not player.jumping:
@@ -201,6 +289,9 @@ while True: #game loop
     if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
         player.velocity_x = PLAYER_VELOCITY_X
         player.direction = "right"
+
+    if keys[pygame.K_x] or keys[pygame.K_SPACE]:
+        player.set_shooting()
 
     move()
     draw()
