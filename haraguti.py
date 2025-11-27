fb7b00be56c7a9f0827ddae83f8b573b1e6f6c19
Igor Rodrigues <igor.rodrigues@exati.com.br>
2025-11-26 23:24:40 -0300

    feat: lago e baldes movimentacoes e estados

--- haraguti.py
+++ haraguti.py
@@ -21,13 +21,25 @@ PLAYER_BULLET_HEIGHT = 12
 PLAYER_BULLET_VELOCITY = 8
 
 HEALTH_WIDTH = 64
-HEALDTH_HEIGHT = 4
+HEALTH_HEIGHT = 4
 
 # plant variables
 PLANT_X = GAME_WIDTH / 2 - PLAYER_WIDTH / 2
 PLANT_Y = GAME_HEIGHT / 2 - PLAYER_HEIGHT / 2
 PLANT_WIDTH = 64
 PLANT_HEIGHT = 64
+
+# lake variables
+LAKE_WIDTH = 256
+LAKE_HEIGHT = 128
+LAKE_X = GAME_WIDTH * 2/3 - LAKE_WIDTH / 2
+LAKE_Y = - LAKE_HEIGHT / 3
+
+# bucket variables
+BUCKET_WIDTH = 32
+BUCKET_HEIGHT = BUCKET_WIDTH
+BUCKET_X = LAKE_X - BUCKET_WIDTH - 10
+BUCKET_Y = LAKE_Y + LAKE_HEIGHT - BUCKET_HEIGHT - 10
 
 # enemy variables
 NOIA_WIDTH = 42
@@ -52,6 +64,11 @@ player_image_shoot_back = load_image("zinho-back-shoot.png", (PLAYER_WIDTH,PLAYE
 player_shoot_image = load_image("zinho-down-shoot.png", (PLAYER_WIDTH, PLAYER_HEIGHT))
 plant_image1 = load_image("planta-01.png", (PLANT_WIDTH, PLANT_HEIGHT))
 plant_image2 = load_image("planta-02.png", (PLANT_WIDTH, PLANT_HEIGHT))
+lake_image_empty = load_image("lake-empty.png", (LAKE_WIDTH, LAKE_HEIGHT))
+lake_image_full = load_image("lake-full.png", (LAKE_WIDTH, LAKE_HEIGHT))
+bucket_image_empty = load_image("bucket-empty.png", (BUCKET_WIDTH, BUCKET_HEIGHT))
+bucket_image_full = load_image("bucket-full.png", (BUCKET_WIDTH, BUCKET_HEIGHT))
+
 pygame.init()
 window = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
 pygame.display.set_caption("A Última Semente")
@@ -72,6 +89,7 @@ class Player(pygame.Rect):
         self.health = self.max_health
         self.shooting = False
         self.bullets = []
+        self.with_bucket = True
 
     def update_image(self):
         if self.shooting == True:
@@ -119,9 +137,31 @@ class Plant(pygame.Rect):
         elif self.level == 2:
             self.image = plant_image2
 
-        
+class Lake(pygame.Rect):
+    def __init__(self):
+        super().__init__(LAKE_X, LAKE_Y, LAKE_WIDTH, LAKE_HEIGHT)
+        self.image = lake_image_full
+        self.full = True
+
+    def update_image(self):
+        if self.full == True:
+            self.image = lake_image_full
+        else:
+            self.image = lake_image_empty
+
+class Bucket(pygame.Rect):
+    def __init__(self):
+        super().__init__(BUCKET_X, BUCKET_Y, BUCKET_WIDTH, BUCKET_HEIGHT)
+        self.image = bucket_image_empty
+        self.full = False
+
+    def update_image(self):
+        if self.full == True:
+            self.image = bucket_image_full
+        else:
+            self.image = bucket_image_empty
 def move():
-    # x movement
+    # player x movement
     if player.direction == "left" and player.velocity_x < 0:
         player.velocity_x += FRICTION
     elif player.direction == "right" and player.velocity_x > 0:
@@ -135,7 +175,7 @@ def move():
     elif player.x + player.width > GAME_WIDTH:
         player.x = GAME_WIDTH - player.width
     
-    # y movement
+    # player y movement
     if player.direction == "down" and player.velocity_y > 0:
         player.velocity_y -= FRICTION
     elif player.direction == "up" and player.velocity_y < 0:
@@ -149,22 +189,39 @@ def move():
     elif player.y + player.height > GAME_HEIGHT:
         player.y = GAME_HEIGHT - player.height
 
+    # bucket 
+    if player.with_bucket == True:
+        bucket.x = player.x
+        bucket.y = player.y + player.height  - bucket.height
         
 # start game
 player = Player()
 plant = Plant()
+lake = Lake()
+bucket = Bucket()
 
 def draw():
     window.blit(background_image, (0, 0))
     window.blit(player.image, (player.x, player.y))
 
+    # lake
+    lake.update_image()
+    window.blit(lake.image, lake)
+    
     # plant
     plant.update_image()
     window.blit(plant.image, plant)
 
-    # player
+    # player and bucket
     player.update_image()
-    window.blit(player.image, player)
+    bucket.update_image()
+    if player.with_bucket == True and player.direction in ["left", "down", "right"]:
+        window.blit(player.image, player)
+        window.blit(bucket.image, bucket)
+    else:
+        window.blit(bucket.image, bucket)
+        window.blit(player.image, player)
+
 while True: #game loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
